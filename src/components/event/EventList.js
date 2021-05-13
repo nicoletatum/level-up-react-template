1f68be07c2560b205d459603b947c262bdba8103
nicoletatum <nicoletatum@outlook.com>
2021-05-13 15:38:31 -0500

    one ch left

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from "react"
 import { EventContext } from "./EventProvider.js"
+// import { ProfileContext } from "../auth/ProfileProvider.js"
 import { useHistory } from "react-router-dom"
 
-export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+export const EventList = () => {
     const history = useHistory()
+    const { events, getEvents, joinEvent, leaveEvent } = useContext(EventContext)
+    // const { profiles, getProfile} = useContext(ProfileContext)
 
     useEffect(() => {
         getEvents()
@@ -13,31 +15,31 @@ export const EventList = (props) => {
     return (
         <article className="events">
             <header className="events__header">
-                <h1>Level Up Events</h1>
+                <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
             </header>
-            <button className="btn btn-2 btn-sep icon-create"
-                onClick={() => {
-                    history.push({ pathname: "/events/new" })
-                }}
-            >Register New Event</button>
             {
                 events.map(event => {
+                    // const attending = profile.events.some(evt => evt.id === event.id)
                     return <section key={event.id} className="registration">
-                        <div className="registration__game">{event.game.name}</div>
+                        <div className="registration__game">{event.game.title}</div>
                         <div>{event.description}</div>
                         <div>
-                            {
-                                new Date(event.date).toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
-                            }
-                            @ {event.time}
+                            {event.date} @ {event.time}
                         </div>
-                        <br />
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id)}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id)}
+                                    >Join</button>
+                        }
                     </section>
                 })
             }
