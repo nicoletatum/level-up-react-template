c863361c6ff573e4cb42f3362a925cd66dccd393
nicoletatum <nicoletatum@outlook.com>
2021-05-13 20:12:50 -0500

    taking a break

--- src/components/game/GameProvider.js
+++ src/components/game/GameProvider.js
@@ -15,8 +15,27 @@ export const GameProvider = (props) => {
             .then(response => response.json())
             .then(setGames)
     }
+    const getGameById = (gameId) => {
+        return fetch(`http://localhost:8000/games/${gameId}`, {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+        })
+            .then(response => response.json())
+    }
     const createGame = (game) => {
         return fetch("http://localhost:8000/games", {
+            method:"POST",
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(game)
+        })
+        .then(getGames)
+    }
+    const editGame = (game) => {
+        return fetch(`http://localhost:8000/games/${game.idd}/edit`, {
             method:"POST",
             headers:{
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`,
@@ -38,7 +57,7 @@ export const GameProvider = (props) => {
     }
 
     return (
-        <GameContext.Provider value={{ games, getGames, createGame, gameTypes, getGameTypes }} >
+        <GameContext.Provider value={{ games, getGames, createGame, gameTypes, getGameTypes, getGameById, editGame }} >
             { props.children }
         </GameContext.Provider>
     )
